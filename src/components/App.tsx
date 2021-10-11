f60400315fb49869267a7e6d51b39b97facf5814
Evgeniy <eilinskih92@gmail.com>
2021-10-11 15:31:31 +0300

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import a from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={a.AppWrapper}>
+      <div className={a.AppContainer}>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
